b30628a8bfd031da4ace2d058ee5f1a7f20e39d3
vagrant <vagrant@homestead>
2017-05-02 16:16:08 +0000

    Sync with master

--- resources/views/emails/partials/features-left-radio-right.blade.php
+++ resources/views/emails/partials/features-left-radio-right.blade.php
@@ -13,7 +13,7 @@
 				</ul>
 			</td>
 			<td width="50%" width="50%" style="font-family: sans-serif;font-size: 16px !important;vertical-align: bottom; text-align: center">
-				<img src="http://mailer.wholehousetransmitter.com/images/radio-small-270.jpg" alt="FM Transmitter" />
+				<img src="https://s3.us-east-2.amazonaws.com/mailman-media/radio-small-270.jpg" alt="FM Transmitter" />
 			</td>
 		</tr>
 	</tbody>
